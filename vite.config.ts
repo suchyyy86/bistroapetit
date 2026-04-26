25dc5fbcd4424c6cc5c48a8ea3552122c27c32df
Mariusz Suchanek <mariusz.suchanek@railsformers.com>
2026-04-26 15:06:07 +0200

    fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
+    nitro(),
     viteReact(),
     tailwindcss(),
     tsConfigPaths(),
